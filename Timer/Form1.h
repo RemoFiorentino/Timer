b0e77780b61635be2d98e51333584d38ac324a52
Remo Fiorentino <rfiorentino@uninorte.edu.co>
2016-02-13 19:38:22 -0500

    Error with mouse position

--- Timer/Form1.h
+++ Timer/Form1.h
@@ -34,6 +34,9 @@ namespace Timer1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	protected: 
+	private: System::Windows::Forms::Timer^  timer1;
 
 	protected: 
 
@@ -52,30 +55,71 @@ namespace Timer1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Arial", 72));
+			this->label1->Location = System::Drawing::Point(1, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(417, 107);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"00:00:00";
+			// 
+			// timer1
+			// 
+			this->timer1->Enabled = true;
+			this->timer1->Interval = 1000;
+			this->timer1->Tick += gcnew System::EventHandler(this, &Form1::timer1_Tick);
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(573, 335);
+			this->ClientSize = System::Drawing::Size(415, 111);
+			this->Controls->Add(this->label1);
 			this->Name = L"Form1";
-			this->Text = L"Form1";
-			this->Click += gcnew System::EventHandler(this, &Form1::form_click);
+			this->Text = L"Timer";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void form_click(System::Object^  sender, System::EventArgs^  e) {
-				int x = System::Windows::Forms::Control::MousePosition.X;
-
-				int y = System::Windows::Forms::Control::MousePosition.Y;
-
-				int xe = System::Windows::Forms::Control::Location.X+8;
-
-				int ye = System::Windows::Forms::Control::Location.Y+30;
-
-				MessageBox::Show((x-xe)+" - "+(y-ye));
+	private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
+				int h=0,m=0,s=0;
+				array<String^>^ time = label1->Text->Split(':');
+				h = Int32::Parse(time[0]);
+				m = Int32::Parse(time[1]);
+				s = Int32::Parse(time[2]);
+				s = s++;
+				if(s == 60){
+					s = 0;
+					m = m++;
+					if(m == 60){
+						m = 0;
+						h = h++;
+					}
+				}
+				if(h < 10){
+					time[0] = "0"+h;
+				}else{
+					time[0] = ""+h;
+				}
+				if(m < 10){
+					time[1] = "0"+m;
+				}else{
+					time[1] = ""+m;
+				}
+				if(s < 10){
+					time[2] = "0"+s;
+				}else{
+					time[2] = ""+s;
+				}
+				label1->Text = time[0]+":"+time[1]+":"+time[2];
 			 }
 	};
 }
